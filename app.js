0a9bc05bcc1a7455e2f81b7d92003d59b18fcd1b
nerdyash <ythakkar2016@gmail.com>
2017-03-10 00:20:03 -0500

    7th

--- app.js
+++ app.js
@@ -12,13 +12,10 @@ var app = angular.module('portfolio', ['ngRoute', 'ngSanitize'])
             .when('/contact', {
                 templateUrl: 'view/contact.html'
             })
-<<<<<<< HEAD
             .when('/project', {
                 templateUrl: 'view/project.html',
                 controller: 'ProCtrl'
             })
-=======
->>>>>>> b34ac3facaeb0938b6d5f572c828970d19623661
             .when('/experience', {
                 templateUrl:'view/experience.html',
                 controller: 'ExpCtrl'
